8aaff747b944700f4149fa8762cd8483b443c73e
Nick Bernard & Sergii Mertsalov <student@epicodus.com>
2014-07-14 16:56:59 -0700

    Added checkbox fade out

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,22 @@ $(document).ready(function(){
 
     var inputtedTask = $("input#task-name").val();
     var inputtedDate = $("input#due-date").val();
+    var checkedStatus = $("input.checkbox").val();
 
-    var task = { name: inputtedTask, date: inputtedDate };
+    var task = { name: inputtedTask, date: inputtedDate, status: checkedStatus };
+    console.log(task);
 
-    $("tbody.tasks").append("<tr>" + "<td class='tasks'>" + task.name + "</td>" + "<td class='due'>" + task.date + "</td>" + "<td>" + "<input type='checkbox'>" + "</tr>");
-
-
-
+    $("tbody.tasks").append("<tr>" + "<td class='tasks'>" + task.name + "</td>" + "<td class='due'>" + task.date + "</td>" + "<td>" + "<input type='checkbox' class='checkbox'>" + "</td>" + "</tr>");
     $("input#task-name").val("");
+    $("input#due-date").val("");
+
+    $("input.checkbox").click(function() {
+      $(this).closest("tr").fadeOut();
+    });
   });
+
   $("button#clear").click(function(){
     $("tbody.tasks").empty();
   });
+
 });
